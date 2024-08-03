96dd6f024a666366bdca297638bb9fade15df84c
pnd-pch <peter.chovanec.p@gmail.com>
2024-08-03 07:53:29 +0200

    added chat history

--- chatbot.py
+++ chatbot.py
@@ -8,10 +8,19 @@ llm = ChatOpenAI(model = "gpt-3.5-turbo")
 
 st.title("TestAI")
 
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+
 if prompt := st.chat_input("Write here"):
     st.chat_message("user").markdown(prompt)
+    st.session_state.messages.append({"role":"user","content":prompt})
     ai_resp = llm(prompt)
     if ai_resp:
         st.chat_message("ai").write(ai_resp.content)
-    
+        st.session_state.messages.append({"role":"ai","content":ai_resp.content})
 
